54bdc1d988aeecdad17a10a7e02b761b481c3d39
alivia-23 <guin.al@husky.neu.edu>
2023-10-17 12:57:11 -0700

    delete folder

--- week2ProgrammingExercises/findPerimeterOfImage.js
+++
@@ -1,9 +0,0 @@
-function perimeter(imageName) {
-    var someImg = new SimpleImage(imageName);
-    var height = someImg.getHeight();
-    var width = someImg.getWidth();
-    var result = 2 * height + 2 * width;
-    return result;
-}
-
-console.log(perimeter("rodger.png"));
--- week2ProgrammingExercises/reformatName.js
+++
@@ -1,9 +0,0 @@
-function reformatName(first, last) {
-    var result = last + "," + first;
-    return result;
-}
-
-var result = reformatName("Susan", "Rodger");
-console.log(result);
-result = reformatName("Robert", "Duvall");
-console.log(result);
